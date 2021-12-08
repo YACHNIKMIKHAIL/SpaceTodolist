c38916645390e2b0c736ba643c14fbec10556026
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-08 16:15:38 +0300

    Updates

---
+++ precache-manifest.91cc05b674b7d112bcf2286a21318198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c416b2af65a5475cd5bac723c08e50e4",
+    "url": "/SpaceTodolist/index.html"
+  },
+  {
+    "revision": "7a4b09b43f1d430d1b1c",
+    "url": "/SpaceTodolist/static/css/main.46dd23f8.chunk.css"
+  },
+  {
+    "revision": "4689bacad20989fe6d60",
+    "url": "/SpaceTodolist/static/js/2.72ee9e83.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/SpaceTodolist/static/js/2.72ee9e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4b09b43f1d430d1b1c",
+    "url": "/SpaceTodolist/static/js/main.e226f199.chunk.js"
+  },
+  {
+    "revision": "290c6b155a6cc117abbc",
+    "url": "/SpaceTodolist/static/js/runtime-main.55b9e915.js"
+  },
+  {
+    "revision": "5940d32bcb65991cb0f59f598a6e30a5",
+    "url": "/SpaceTodolist/static/media/wallpaperflare.com_wallpaper.5940d32b.jpg"
+  }
+]);
