4e3f7aa9ddc320a7a7122b17167a367a789ec5fd
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-12 12:26:40 +0300

    Undependent checkbox finale

--- src/Components/Map/TasksMap.tsx
+++ src/Components/Map/TasksMap.tsx
@@ -6,24 +6,22 @@ import {Checkbox} from "../Checkbox/Checkbox";
 
 type TasksMapType = {
     tasks: Array<TaskType>
-    changeCheckbox: (id: string, e: ChangeEvent<HTMLInputElement>,todolistID: string) => void
+    changeCheckbox: (id: string, e: ChangeEvent<HTMLInputElement>, todolistID: string) => void
     removeTaskX: (id: string) => void
     id: string
     changeTaskTitle: (id: string, title: string, todolistId: string) => void
     todolistID: string
 }
 export const TasksMap = ({tasks, changeCheckbox, removeTaskX, ...props}: TasksMapType) => {
-    const changeTaskTitle = (title: string) => {
-        props.changeTaskTitle(props.id, title, props.todolistID)
-    }
-    const changeCheckboxX = (id:string,e: ChangeEvent<HTMLInputElement>) => {
-        changeCheckbox(id, e, props.todolistID)
-    }
+    const changeTaskTitle = (title: string) => props.changeTaskTitle(props.id, title, props.todolistID)
+    const changeCheckboxX = (id: string, e: ChangeEvent<HTMLInputElement>) => changeCheckbox(id, e, props.todolistID)
+
     return (
         <ul>{
             tasks.map(m => {
                     return <li key={m.id} className={m.isDone ? 'is-done' : ''}>
-                        <Checkbox isDone={m.isDone} callback={(e: React.ChangeEvent<HTMLInputElement>)=>changeCheckboxX(m.id,e)} />
+                        <Checkbox isDone={m.isDone}
+                                  callback={(e: React.ChangeEvent<HTMLInputElement>) => changeCheckboxX(m.id, e)}/>
                         <EditableSpan title={m.title} onChange={changeTaskTitle}/>
                         <Button name={'x'} callback={() => removeTaskX(m.id)}/>
                     </li>
