ce64e8cf1fba873e79a5a1ba4b22bedde66fe429
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-21 15:40:58 +0300

    second test  commit

--- src/Components/State/TodolistReducer.test.ts
+++ src/Components/State/TodolistReducer.test.ts
@@ -11,8 +11,26 @@ test('correct todolist remove', () => {
         {id: todolist2, title: 'What to buy?', filter: 'all'}
     ]
 
-    const endState = todolistReducer(startState, {type: 'REMOVE_TODO', id: todolist1})
+    const endState = todolistReducer(startState, {
+        type: 'REMOVE_TODO', id: todolist1})
 
     expect(endState.length).toBe(1)
     expect(endState[0].id).toBe(todolist2)
+})
+
+test('correct todolist add', () => {
+    let todolist1 = v1()
+    let todolist2 = v1()
+
+    const startState: Array<TodolitsType> = [
+        {id: todolist1, title: 'What to learn?', filter: 'all'},
+        {id: todolist2, title: 'What to buy?', filter: 'all'}
+    ]
+
+    const endState = todolistReducer(startState, {
+        type: 'ADD_TODO', newTitle: 'Restoran'})
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe('Restoran')
+    expect(endState[2].filter).toBe('all')
 })
--- src/Components/State/TodolistReducer.ts
+++ src/Components/State/TodolistReducer.ts
@@ -1,4 +1,5 @@
 import {TodolitsType} from "../Todolist/Todolist";
+import {v1} from "uuid";
 
 type ActionType = {
     type: string
@@ -9,6 +10,13 @@ export const todolistReducer = (state: Array<TodolitsType>, action: ActionType):
         switch (action.type) {
             case 'REMOVE_TODO': {
                 return state.filter(f => f.id !== action.id)
+            }
+            case 'ADD_TODO': {
+                return [...state, {
+                    id: v1(),
+                    title: action.newTitle,
+                    filter: 'all'
+                }]
             }
             default:
                 return state
