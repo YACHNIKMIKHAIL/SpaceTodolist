6c04b09129cfc4b69959ef729936903042754097
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-01-24 22:14:41 +0300

    snapchots WERY SAD commit

--- src/Components/EditableSpan/EditableSpan.tsx
+++ src/Components/EditableSpan/EditableSpan.tsx
@@ -1,30 +1,30 @@
-import React, {useState} from "react";
+import React, {useCallback, useState} from "react";
 import {TextField} from "@mui/material";
 
 type EditableSpanPropsType = {
     title: string
     onChange: (title: string) => void
 }
-export const EditableSpanMemo = ({onChange, ...props}: EditableSpanPropsType) => {
-    const [title, setTitle] = useState<string>(props.title)
+export const EditableSpanMemo = ({onChange,title}: EditableSpanPropsType) => {
+    const [stateTitle, setStateTitle] = useState<string>(title)
     const [editM, setEditM] = useState<boolean>(false)
 
     console.log('span render')
-    const activateEditM = () => {
+    const activateEditM = useCallback(() => {
         setEditM(true)
-        setTitle(props.title)
-    }
-    const activateViewM = () => {
+        setStateTitle(title)
+    }, [])
+    const activateViewM = useCallback(() => {
         setEditM(false)
-        onChange(title)
-    }
-    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
+        onChange(stateTitle)
+    }, [])
+    const onChangeHandler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => setStateTitle(e.currentTarget.value), [])
 
     return editM
-        ? <TextField id="standard-basic" label="Changes:" variant="standard" value={title} autoFocus
+        ? <TextField id="standard-basic" label="Changes:" variant="standard" value={stateTitle} autoFocus
                      onChange={onChangeHandler} onBlur={activateViewM}
                      style={{color: 'white'}}/>
         : <span onDoubleClick={activateEditM}
-        >{title}</span>
+        >{stateTitle}</span>
 }
 export const EditableSpan = React.memo(EditableSpanMemo)
